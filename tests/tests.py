013c491572c46c5e4005d7a200bbd0f8e7fffd42
Jorge Monforte González <jorge@monforte.tech>
2023-02-01 08:08:31 +0100

    just drafting tests

---
+++ tests/tests.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from html.parser import HTMLParser
+from htmlcalendar import htmlday, htmlmonth, get_months
+
+class CalendarParser(HTMLParser):
+    pass
+
+class HtmlDayTestCase(TestCase):
+    pass
+
+class HtmlMonthTestCase(TestCase):
+    pass
