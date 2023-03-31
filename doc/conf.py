ee552d5b15d9b8acd28d8f322e0be818210f32f3
Jorge Monforte González <jorge@monforte.tech>
2023-03-31 10:52:46 +0200

    doc fixes

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.abspath('..'), 'html-calendar'))
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'html-calendar'
 copyright = '2023, Jorge Monforte González'
